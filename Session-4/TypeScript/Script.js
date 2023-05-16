e484dbc58cb94a7638ff9929e53cb52fc498ff34
rajkamal05 <rajkamal.gupta454@gmail.com>
2023-05-16 11:09:18 +0530

    hi

---
+++ Session-4/TypeScript/Script.js
@@ -0,0 +1,18 @@
+var fname = "Rajkamal";
+var age = 30;
+var city = "Mumbai";
+console.log(fname);
+console.log(age);
+console.log(city);
+console.log("Hello");
+function check(value) {
+    if (value) {
+        console.log("Perform an Action");
+    }
+    else {
+        console.log("Can't Perform Anything");
+    }
+}
+check("true");
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
--- Session-4/TypeScript/Script.ts
+++ Session-4/TypeScript/Script.ts
@@ -1,6 +1,27 @@
 let fname = "Rajkamal"
 let age = 30
 let city = "Mumbai"
+console.log(fname)
+console.log(age)
+console.log(city)
+
+console.log("Hello")
+
+
+
+function check(value){
+    if(value){
+        console.log("Perform an Action")
+    }
+    else
+    {
+        console.log("Can't Perform Anything")
+    }
+}
+
+
+check("true")
+
 
 const inputs = document.querySelectorAll('input')
 console.log(inputs)
