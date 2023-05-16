ab4bb92b39e1543d4763dc10bf9677089d55b482
rajkamal05 <rajkamal.gupta454@gmail.com>
2023-05-16 10:24:17 +0530

    add

---
+++ Session-4/ResonForNewLang.js
@@ -0,0 +1,12 @@
+function check(value){
+    if(value == true){
+        console.log("Perform an Action")
+    }
+    else
+    {
+        console.log("Can't Perform Anything")
+    }
+}
+
+
+check("true")
---
+++ Session-4/TypeScript/Script.ts
@@ -0,0 +1,6 @@
+let fname = "Rajkamal"
+let age = 30
+let city = "Mumbai"
+
+const inputs = document.querySelectorAll('input')
+console.log(inputs)
